f11e77a073094753dc58faae693c5fb9f68fb152
Alexandra Matveeva <sm1991611@gmail.com>
2025-04-14 00:52:27 +0700

    fix(interface): minor adjustments for Paste functionality

--- resources/views/paste/login.blade.php
+++ resources/views/paste/login.blade.php
@@ -11,7 +11,7 @@
                 {{ session('errors') }}
             </div>
         @endif
-        <h1 class="text-center mb-4">Вход</h1>
+        <h1 class="text-center mb-4">Вход в аккаунт Pastebin</h1>
         <form action="{{ route('paste.user.store') }}" method="POST">
             @csrf
             <div class="form-group mb-2">
@@ -21,6 +21,9 @@
             <div class="form-group mb-2">
                 <label for="user_password">Пароль</label>
                 <input class="form-control bg-dark" type="password" id="user_password" name="user_password" required>
+            </div>
+            <div class="form-group mb-2">
+                <small>После авторизации в аккаунте Pastebin вы можете публиковать пасты от своего имени. Опубликовать пасту можно перейдя по ссылки в пункте меню или <a class="link-info" href="{{ route('paste.index') }}">нажав сюда.</a></small>
             </div>
             <button type="submit" class="btn btn-primary">Войти в аккаунт</button>
         </form>
