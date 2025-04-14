03c8106e2b5523b3be60b397c3eff1c6dc782b36
Alexandra Matveeva <sm1991611@gmail.com>
2025-04-14 15:44:33 +0700

    feat(ui): add Pastebin account info to user profile page.

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -7,6 +7,14 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            @if(Auth::user()->api_key)
+                <div class="p-4 sm:p-8 dark:bg-gray-800 shadow sm:rounded-lg">
+                    <div class="max-w-xl">
+                        @include('profile.partials.show_pastebin_account')
+                    </div>
+                </div>
+            @endif
+
             <div class="p-4 sm:p-8 dark:bg-gray-800 shadow sm:rounded-lg">
                 <div class="max-w-xl">
                     @include('profile.partials.update-profile-information-form')
---
+++ resources/views/profile/partials/show_pastebin_account.blade.php
@@ -0,0 +1,11 @@
+<section>
+    <header>
+        <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+            {{ __('Информация об авторизации в аккаунте Pastebin') }}
+        </h2>
+
+        <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+            {{ __('Вы успешно вошли в аккаунт Pastebin.') }}
+        </p>
+    </header>
+</section>
